ea8679e756b913e59d516a73bfbd3886f36138e8
sidharth atri <sidharthatri@gmail.com>
2020-05-02 08:15:15 +0530

    finshing sign-in handleSubmit method, sign-in and sign-out component completed.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 export default class SignIn extends Component {
   constructor(props) {
@@ -16,8 +16,17 @@ export default class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
 
     this.setState({ email: "", password: "" });
   };
@@ -41,7 +50,6 @@ export default class SignIn extends Component {
             handleChange={this.handleChange}
             value={this.state.email}
             label="email"
-            required
           />
 
           <FormInput
@@ -50,7 +58,6 @@ export default class SignIn extends Component {
             handleChange={this.handleChange}
             value={this.state.password}
             label="password"
-            required
           />
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
